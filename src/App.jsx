adce574e4e2a877eccf6f60de6de5b8c8f9e655f
nanakwametuffour <nanakwametuffour44@gmail.com>
2024-09-19 08:01:10 -0700

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <div className='text-red-700'>App</div>
+    <>
+    <BrowserRouter>
+     <Routes>
+      <Route element={<Layout/>}>
+        <Route path='/' element={<Home/>}/>
+      </Route>
+     </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
