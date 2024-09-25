2c46f8b774e309f3d8539aa3215b2f6387478fc5
nanakwametuffour <nanakwametuffour44@gmail.com>
2024-09-25 04:35:34 -0700

    working on the menu component and map component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,56 @@
-import React from 'react'
-import { FiAlignCenter } from 'react-icons/fi'
-import { GiShoppingCart } from 'react-icons/gi'
-import img from '../assets/chick.jpg'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { setCartOpen, setIsOpen } from '../redux/cart/cartSlice'
+import React, { useState } from "react";
+import { FiAlignCenter } from "react-icons/fi";
+import { GiShoppingCart } from "react-icons/gi";
+import img from "../assets/chick.jpg";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setCartOpen, setIsOpen } from "../redux/cart/cartSlice";
+import { heroMenu } from "../assets/menuData";
+import HeroMenu from "./HeroMenu";
+
 export default function Header() {
-    const dispatch =useDispatch()
-    const product = useSelector((state)=>state.product.sideState)
-    const addProduct =useSelector((state)=>state.product.cartProduct)
-           
-           
-   
+  const dispatch = useDispatch();
+
+  const addProduct = useSelector((state) => state.product.cartProduct);
+
   return (
-    <div className='bg-white shadow sticky top-0 px-2 z-10'>
-        <div className="w-full max-w-6xl mx-auto p-3 flex justify-between items-center">
-            <div className="flex justify-center items-center gap-2">
-            <FiAlignCenter onClick={()=>dispatch(setIsOpen())} className='text-4xl font-semibold active:scale-105 cursor-pointer'/>
-               <Link to={'/'}>
+    <div className="bg-white shadow sticky top-0 px-2 z-10">
+      <div className="w-full max-w-6xl mx-auto p-3 flex justify-between items-center">
+        <div className="flex justify-center items-center gap-2">
+          <FiAlignCenter
+            onClick={() => dispatch(setIsOpen())}
+            className="text-4xl font-semibold active:scale-105 cursor-pointer"
+          />
+          <Link to={"/"}>
             <div className="flex justify-center items-center">
-              <h1 className='text-red-600 text-sm lg:text-2xl font-semibold'>YUMMY</h1>
-               <img src={img} alt="img" className='w-10 h-10 lg:w-14 lg:h-14 object-cover'/>
+              <h1 className="text-red-600 text-sm lg:text-2xl font-semibold">
+                YUMMY
+              </h1>
+              <img
+                src={img}
+                alt="img"
+                className="w-10 h-10 lg:w-14 lg:h-14 object-cover"
+              />
             </div>
-               </Link>
-            </div>
-            <div className=" flex  w-auto justify-center border p-2 rounded-lg">
-                <input type="text" className='w-24 sm:w-64 focus:outline-none bg-transparent placeholder:text-gray' placeholder='Search meal..' />
-            </div>
-            <div onClick={()=>dispatch(setCartOpen())}  className="  relative active:scale-105 cursor-pointer">
-            <GiShoppingCart  className=' text-5xl text-black font-semibold'/>
-              <span className='w-5 h-5 bg-red-700 flex items-center justify-center rounded-full text-white absolute top-0 right-0'>
-                 {
-                  addProduct.length > 0 ? addProduct.length : 0
-                 }
-                </span>
-            </div>
+          </Link>
         </div>
+        {/* <div className=" flex  w-auto justify-center border p-2 rounded-lg">
+          <input
+            type="text"
+            className="w-24 sm:w-64 focus:outline-none bg-transparent placeholder:text-gray"
+            placeholder="Search meal.."
+          />
+        </div> */}
+        <div
+          onClick={() => dispatch(setCartOpen())}
+          className="  relative active:scale-105 cursor-pointer"
+        >
+          <GiShoppingCart className=" text-5xl text-black font-semibold" />
+          <span className="w-5 h-5 bg-red-700 flex items-center justify-center rounded-full text-white absolute top-0 right-0">
+            {addProduct.length > 0 ? addProduct.length : 0}
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { GrMapLocation } from "react-icons/gr";
+import { LiaTimesSolid } from "react-icons/lia";
+import { RiMenuFold2Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import Map from "../components/Map";
 
 export default function Menu() {
+  const [locationMap, setLocationMap] = useState(false);
   return (
-    <div>Menu</div>
-  )
+    <div className="w-full h-auto flex flex-col gap-5 top-0 py-9 px-6 lg:px-10">
+      <div className="  w-full h-auto flex justify-between items-center">
+        <div className=" relative w-60 lg:w-[600px] border rounded-lg px-2 p-4">
+          <input
+            type="text"
+            placeholder="Search Meal...."
+            className="w-full pl-11 focus:outline-none px-3 "
+          />
+          <div className="flex justify-between absolute w-full top-3 px-4 text-2xl">
+            <CiSearch />
+            <LiaTimesSolid />
+          </div>
+        </div>
+        <div className="">
+          {!locationMap ? (
+            <GrMapLocation onClick={() => setLocationMap(true)} className="text-6xl border px-3 cursor-pointer font-semibold rounded-lg"/>
+          ) : (
+            <RiMenuFold2Line onClick={() => setLocationMap(false)} className="text-6xl border px-3 cursor-pointer font-bolder rounded-lg"/>
+          )}
+        </div>
+      </div>
+      <div className="">button header</div>
+      <div className="flex-row lg:flex w-full justify-center items-center px-5 gap-5">
+        <div className="">
+          menu
+        </div>
+        <div className={` ${!locationMap ? "hidden" : "block"}`}>
+        <Map/> 
+        </div>
+      </div>
+    </div>
+  );
 }
